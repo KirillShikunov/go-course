5130be7223c04fcea8e7ce05ac9e610d81ff65f8
Kyrylo Shykunov <kyrylo.shykunov@quarks.tech>
2024-04-10 22:37:16 +0300

    Did homework for the 11 lesson

---
+++ 11_regex/search/searcher.go
@@ -0,0 +1,37 @@
+package search
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func FindInFile(fileName string, regex string) ([]string, error) {
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, fmt.Errorf("помилка при відкритті файлу: %w", err)
+	}
+
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println("Не вдалося закрити файл:", err)
+		}
+	}(file)
+
+	regexCompile := regexp.MustCompile(regex)
+
+	scanner := bufio.NewScanner(file)
+	var result []string
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		matches := regexCompile.FindAllString(line, -1)
+		for _, match := range matches {
+			result = append(result, match)
+		}
+	}
+
+	return result, nil
+}
